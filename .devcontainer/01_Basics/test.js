fd6191cbd6011cb7c88c58732a6042430ab6ff56
rahulaharma <137466238+rahulaharma@users.noreply.github.com>
2024-01-03 11:44:19 +0000

    done

--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,1 +1,10 @@
-console.log("rahul")
+const fruits = [{
+name:"rahul"
+}, '2'];
+
+// Create a copy using spread syntax.
+const fruitsCopy = [...fruits];
+
+fruitsCopy[0].name="aayush"
+console.log(fruitsCopy);
+console.log(fruits);
